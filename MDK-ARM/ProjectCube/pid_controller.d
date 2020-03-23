55471132f1f3ad10edbfd6aa522084879bc01a9d
Tobias Pucher <tobiaspucher@gmail.com>
2020-03-23 11:34:26 +0100

    Fuzzy entfernt, compiliert: 0 errors; 5warnings;

--- MDK-ARM/ProjectCube/pid_controller.d
+++ MDK-ARM/ProjectCube/pid_controller.d
@@ -1,4 +1,4 @@
 projectcube\pid_controller.o: pid_controller.c
 projectcube\pid_controller.o: ../Inc/pid_controller.h
-projectcube\pid_controller.o: D:\Diplomarbeit\Keilv5\ARM\ARMCC\Bin\..\include\stdint.h
-projectcube\pid_controller.o: D:\Diplomarbeit\Keilv5\ARM\ARMCC\Bin\..\include\stdbool.h
+projectcube\pid_controller.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+projectcube\pid_controller.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
